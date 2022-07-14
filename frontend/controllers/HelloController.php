19997590860913cd66f603923322d91e8fcb8669
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-07-13 22:35:44 -0300

    setup basic HelloController and view

---
+++ frontend/controllers/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\base\Controller;
+
+class HelloController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
