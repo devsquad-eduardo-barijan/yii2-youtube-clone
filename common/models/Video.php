8952f9a92368949d00d0593429d9b7a41a89f9db
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-07-15 22:03:02 -0300

    add default rules for status and has_thumbnail

--- common/models/Video.php
+++ common/models/Video.php
@@ -26,6 +26,9 @@ use yii\web\UploadedFile;
  */
 class Video extends \yii\db\ActiveRecord
 {
+    const STATUS_UNLISTED = 0;
+    const STATUS_PUBLISHED = 1;
+
     /** @var UploadedFile */
     public $video;
 
@@ -60,6 +63,8 @@ class Video extends \yii\db\ActiveRecord
             [['video_id'], 'string', 'max' => 16],
             [['title', 'tags', 'video_name'], 'string', 'max' => 512],
             [['video_id'], 'unique'],
+            ['has_thumbnail', 'default', 'value' => 0],
+            ['status', 'default', 'value' => self::STATUS_UNLISTED],
             [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
         ];
     }
